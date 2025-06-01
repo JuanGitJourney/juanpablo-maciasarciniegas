619f178b33b3744e4874326592308b7ee5aa4f38
juan-macias <juan.macias@justeattakeaway.com>
2025-06-01 19:29:35 +0200

    Suppor multiple browser testing

--- good-budget-e2e-tests/playwright.config.ts
+++ good-budget-e2e-tests/playwright.config.ts
@@ -1,11 +1,25 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests/e2e',
   use: {
     headless: process.env.CI === 'true' || process.env.DOCKER === 'true',
   },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'safari',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
 });
